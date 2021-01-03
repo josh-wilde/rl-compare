3dd2cfc6b1be6d8f2852072ffca352d055f39ae4
josh-wilde <josh.t.wilde@gmail.com>
2021-01-02 19:25:32 -0500

    debugged n-step sarsa, no reporting

--- EpisodicNStepSarsa.py
+++ EpisodicNStepSarsa.py
@@ -28,10 +28,12 @@
 import click
 import gym
 import numpy as np
-from math import log, ceil
-from tiles3 import IHT, tiles
+from math import log, ceil, exp
+from tiles3 import IHT
 
 from QFunctions import LinearTilingQApproximator
+
+# python EpisodicNStepSarsa.py --episodes 1 --alpha 0.03 --init_epsilon 0.1 --eps_decay_factor 0.0 --n 1 --gamma 0.9 --tile_resolution 8 --n_tilings 8 --d 3 --render
 
 @click.command()
 @click.option('--episodes', default=10)
@@ -52,32 +54,33 @@ def main(episodes, alpha, init_epsilon, eps_decay_factor,
 
     # Initialize the hash table to store the tiling
     n_tiles = tile_resolution ** d * n_tilings * n_actions
-    iht = IHT(2**(1+ceil(log(n_tiles, 2))))
+    iht = IHT(2**(1+ceil(log(n_tiles, 2)))) # should be double the hash table size that we need
 
     # Initialize the Q function
-    # Initialize arrays to store actions and states
-    # Index 1 corresponds to t = 0
     q_hat = LinearTilingQApproximator(iht, n_tilings, tile_resolution)
-    A = np.zeros(n) # Storing the prior n+1 actions. N+1 just so indices line up
-    S = np.zeros((n,d)) # Storing the prior n+1 states
-    R = np.zeros(n) # storing prior n rewards
+
+    # Initialize arrays to store actions, states and rewards
+    # n-step SARSA means storing current info AND info for n more steps
+    A = np.zeros(n+1, dtype=int)
+    S = np.zeros((n+1,d)) # each row is either [position, velocity] or [position, velocity, acceleration]
+    R = np.zeros(n+1)
 
     # Loop over episodes
     for episode in range(episodes):
         # Initial observation
         # For MountainCar, always starts with 0 velocity and append 0 acceleration
-        observation = env.reset()
+        observation = list(env.reset())
 
         if d == 2:
             S[0] = observation
         else:
             S[0] = observation + [0]
 
-        # Store action based on the initial state
+        # epsilon-greedy action based on initial state
         if np.random.uniform() <= init_epsilon:
             A[0] = env.action_space.sample()
         else:
-            A[0] = np.argmax([q_hat(S[0], a) for a in range(n_action)])
+            A[0] = np.argmax([q_hat(S[0], a) for a in range(n_actions)])
 
         # Set termination time to infinity to start
         # Initialize time counter
@@ -87,6 +90,11 @@ def main(episodes, alpha, init_epsilon, eps_decay_factor,
         # render
         if render: env.render()
 
+        ### Print initial state
+        print('t = {:d}'.format(t))
+        print('S[t] = ' + np.array2string(S[0], precision=2))
+        print('A[0] = {:d}'.format(A[0]))
+
         # Loop over time periods within an episode
         while True:
 
@@ -94,11 +102,13 @@ def main(episodes, alpha, init_epsilon, eps_decay_factor,
             # Store the next state and reward
             if t < T:
                 observation, reward, done, info = env.step(A[t % (n+1)])
+                if render: env.render()
+
                 R[(t+1) % (n+1)] = reward
                 if d == 2:
-                    S[(t+1) % (n+1)] = observation
+                    S[(t+1) % (n+1)] = list(observation)
                 else:
-                    S[(t+1) % (n+1)] = observation + [observation[1] - S[t % (n+1), 1]]
+                    S[(t+1) % (n+1)] = list(observation) + [observation[1] - S[t % (n+1), 1]]
                 if done:
                     T = t + 1
                 else:
@@ -106,16 +116,25 @@ def main(episodes, alpha, init_epsilon, eps_decay_factor,
                     if np.random.uniform() <= epsilon:
                         A[(t+1) % (n+1)] = env.action_space.sample()
                     else:
-                        A[(t+1) % (n+1)] = np.argmax([q_hat(S[(t+1) % (n+1)], a) for a in range(n_action)])
+                        A[(t+1) % (n+1)] = np.argmax([q_hat(S[(t+1) % (n+1)], a) for a in range(n_actions)])
+
+                ### Print state for t + 1
+                print('After taking A[{:d}], the info for t+1 = {:d} is:'.format(t, t+1))
+                print('t + 1 = {:d}'.format(t+1))
+                print('S[t+1] = ' + np.array2string(S[(t+1) % (n+1)], precision=2))
+                print('R[t+1] = {:f}'.format(R[(t+1) % (n+1)]))
+                print('A[t+1] chosen based on S[t+1] and R[t+1].')
+                print('A[t+1] = {:d}'.format(A[(t+1) % (n+1)]))
 
             # Set the period for which we are updating the weights
+            # E.g. if n = 1, then can start updating at t = 0 because we have stored S_1 and R_1
             tau = t - n + 1
 
             # If we are ready to update the first state, then go ahead
             if tau >= 0:
-                # discounted n-step return that is real
+                # discounted n-step return
                 G = sum([gamma**(i-tau-1)*R[(i % (n+1))] for i in range(tau+1, min(tau+n, T) + 1)])
-                # if you haven't terminated within n steps, then add the expected return to go
+                # if you haven't terminated within n steps, then add the estimated return to go
                 if tau + n < T:
                     G = G + gamma**n * q_hat(S[(tau+n) % (n+1)], A[(tau+n) % (n+1)])
                 # Adjust the weights based on gradient of the error
@@ -127,7 +146,10 @@ def main(episodes, alpha, init_epsilon, eps_decay_factor,
                              G - q_hat(S[tau % (n+1)], A[tau % (n+1)]))
 
             if tau == T - 1:
+                print('Exiting at tau = {:d}'.format(tau))
                 break
+
+            t += 1
 
 
 if __name__ == '__main__':
--- QFunctions.py
+++ QFunctions.py
@@ -1,5 +1,6 @@
 # Set up linear q function approximation here
 import numpy as np
+from tiles3 import tiles
 
 class LinearTilingQApproximator():
     def __init__(self, iht, n_tilings, tile_resolution):
@@ -19,12 +20,12 @@ class LinearTilingQApproximator():
                             self.tile_resolution * state[1] / (0.07+0.07)]
         if len(state) == 3:
             state_scaled = state_scaled + [self.tile_resolution * state[2] / (0.14+0.14)]
-            
+
         return tiles(self.iht, self.n_tilings, state_scaled, [action])
 
     def __call__(self, state, action):
         active_tiles = self.get_active_tiles(state, action)
-        return sum([w[t] for t in active_tiles])
+        return sum([self.w[t] for t in active_tiles])
 
     def update(self, state, action, alpha, error):
         active_tiles = self.get_active_tiles(state, action)
